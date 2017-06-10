aa51f9d819f5433e3b828d93deb783f4f4469332
F. Van Dinh <fvandinh@gmail.com>
2017-06-10 01:52:18 +0000

    Controller
    Route fixed, controller created

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,81 @@
 class ContactsController < ApplicationController
+
+    def index
+        @contacts = Contact.all
+    end
+
+    def show
+        @contact = Contact.find(params[:id])
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+ 
+        if @contact.save
+            redirect_to @contact
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @contact = Contact.new
+    end
+
+    def update
+        @contact = Contact.find(params[:id])
+ 
+        if @contact.update(contact_params)
+            redirect_to @contact
+        else
+            render 'edit'
+        end
+    end
+
+    def edit
+        @contact = Contact.find(params[:id])
+    end
+
+    def destroy
+        @contact = Contact.find(params[:id])
+        @contact.destroy
+    
+        redirect_to contacts_path
+    end
+
+    def add_photo
+        @contact = Contact.find(params[:contact_id])
+
+        if @contact.update(photo: params[:photo])            
+            redirect_to @contact
+        else
+            render 'add_photo'
+        end
+    end
+
+    def add_tags
+        @contact = Contact.find(params[:contact_id])
+
+        if @contact.update(photo: params[:name])            
+            redirect_to @contact
+        else
+            render 'add_tags'
+        end
+    end
+
+    def search
+        tags = params[:tags]
+        tags.each do |tag|
+
+        end
+        @contact = Contact.includes(:tags).where(tags: {name: tag})
+
+        @contact.each do |
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:firsname, :lastname, :birthday, :address, )
+        end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :contacts
-  post '/contacts/:id/photo', to: 'contacts#add_photo'
-  post '/contacts/:id/tags', to: 'contacts#add_tags'
-  post '/contacts/search', to: 'contacts#search'
+  root to: 'contacts#index'
+
+  resources :contacts do
+    get '/search', on: :collection, action: 'search'
+    put '/photo', action: 'add_photo'
+    resource :tags, only: :create
+  end
+
 end
